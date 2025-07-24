254d07db11a6365b38d376a1b7e2b1d17d8df5d4
Thomas Degris <244774+tdegris@users.noreply.github.com>
2025-07-24 01:24:12 +0100

    GX headers work for helloworld.

--- examples/helloworld/helloworld.cc
+++ examples/helloworld/helloworld.cc
@@ -1,6 +1,27 @@
+#include <absl/status/statusor.h>
 #include <iostream>
 
+#include "gxdeps/github.com/gx-org/gx/golang/binder/ccgx/cppgx.h"
+#include "gxdeps/github.com/gx-org/xlapjrt/cgx/cgx.cgo.h"
+#include <gxdeps/github.com/gx-org/gx/golang/binder/cgx/cgx.h>
+
+using gxlang::cppgx::Runtime;
+using gxlang::cppgx::ToErrorStatus;
+
+absl::StatusOr<Runtime> NewRuntime() {
+  const auto bld(cgx_builder_new_static_xlapjrt());
+  const auto result(cgx_runtime_new_xlapjrt(bld, "cpu"));
+  if (result.error != cgx_error{}) {
+    return ToErrorStatus(result.error);
+  }
+  return Runtime(result.runtime);
+}
+
 int main() {
-  std::cout << "Hello world" << std::endl;
+  auto rtm(NewRuntime());
+  if (!rtm.ok()) {
+    std::cout << rtm.status() << std::endl;
+  }
+  std::cout << "Runtime ok" << std::endl;
   return 0;
 }
