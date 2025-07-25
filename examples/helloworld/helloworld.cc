83d50245111ecbd5528ae9e3644d9ff8226bb379
Thomas Degris <244774+tdegris@users.noreply.github.com>
2025-07-25 17:18:19 +0100

    C++ source compiles. Still have link errors.

--- examples/helloworld/helloworld.cc
+++ examples/helloworld/helloworld.cc
@@ -2,9 +2,11 @@
 #include <iostream>
 
 #include "gxdeps/github.com/gx-org/gx/golang/binder/ccgx/cppgx.h"
+#include "gxdeps/github.com/gx-org/gx/golang/binder/cgx/cgx.h"
 #include "gxdeps/github.com/gx-org/xlapjrt/cgx/cgx.cgo.h"
-#include <gxdeps/github.com/gx-org/gx/golang/binder/cgx/cgx.h>
+#include "gxdeps/helloworld_go_gx.h"
 
+using std::begin;
 using gxlang::cppgx::Runtime;
 using gxlang::cppgx::ToErrorStatus;
 
@@ -17,11 +19,40 @@ absl::StatusOr<Runtime> NewRuntime() {
   return Runtime(result.runtime);
 }
 
+namespace helloworld {
+namespace {
+
 int main() {
-  auto rtm(NewRuntime());
-  if (!rtm.ok()) {
-    std::cout << rtm.status() << std::endl;
+  auto runtime(NewRuntime());
+  if (!runtime.ok()) {
+    std::cerr << "cannot create runtime: " << runtime.status() << std::endl;
+    return 1;
   }
-  std::cout << "Runtime ok" << std::endl;
+  auto device(runtime->GetDevice(0));
+  if (!runtime.ok()) {
+    std::cerr << "cannot create device: " << device.status() << std::endl;
+    return 1;
+  }
+  auto package(Helloworld::BuildFor(device.value()));
+  if (!runtime.ok()) {
+    std::cerr << "cannot compile package: " << package.status() << std::endl;
+    return 1;
+  }
+  auto hello(package->Hello());
+  if (!hello.ok()) {
+    std::cerr << hello.status() << std::endl;
+  }
+  auto data(hello->Acquire());
+  if (!data.ok()) {
+    std::cerr << data.status() << std::endl;
+  }
+  std::cout << "Hello: [ ";
+  for (float value : data.value()) {
+        std::cout << value << " ";
+  }
+  std::cout << "]" << std::endl;
   return 0;
 }
+
+} // namespace
+} // namespace githubcom::gxorg::ccgx::examples::helloworld
